c76fd88d8e8ccf5870e7997e35d0b0e39b0f0e3e
Rodrigo Suarez <ing.rsuarez@gmail.com>
2023-10-21 21:06:32 -0300

    Adding services on welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -154,29 +154,74 @@
                     <h2 class="font-bold text-xl">SERVICIOS</h2>
                     <br><p>
                     Gracias a la incorporación de equipamiento tecnológico, en nuestro centro realizamos estudios complementarios de diagnóstico:
-                    <li>	Laboratorio Toxicológico y otras determinaciones </li>
-                    <li>	Laboratorios clínicos de rutina </li>
-                    <li>	Electrocardiograma y Ergometría </li>
-                    <li>	Espirometria</li>
-                    <li>	Electroencefalograma </li> 
-                    <li>	Radiología </li>
-                    <li>	Audiometría </li>
-                    <li>	Exámenes Psicotécnicos </li>
-                    <li>	Exámenes Psicométricos </li>
-                    <li>	Informe socio ambiental laboral </li>
+                    </p>
+                    <br>
+                    <p>
+                        <li>	Laboratorio Toxicológico y otras determinaciones </li>
+                        <li>	Laboratorios clínicos de rutina </li>
+                        <li>	Electrocardiograma y Ergometría </li>
+                        <li>	Espirometria</li>
+                        <li>	Electroencefalograma </li> 
+                        <li>	Radiología </li>
+                        <li>	Audiometría </li>
+                        <li>	Exámenes Psicotécnicos </li>
+                        <li>	Exámenes Psicométricos </li>
+                        <li>	Informe socio ambiental laboral </li>
                     </p>
                 </a>
             </div>
             
         </div>
-        
-        {{-- <footer class="container mt-8 ml-8 pb-8 w-screen">
-            <img class="w-20" src="{{asset('img/logo.jpg')}}" alt="">
-            <p class="float-left text-violet-400"><a href="#">Inicio</a></p>
-            <p>&copy; Cimad SAS, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-        </footer> --}}
+        <div class="relative w-screen my-8 flex flex-col-reverse md:flex-row items-start animate-[fadein_2s_ease-in_1]">
+            
+            <div class="relative px-4 mx-4 md:ml-8 mt-8 py-3 lg:w-2/5 bg-gray-100/50 rounded-lg shadow-md">
+                <a href="#">
+                <h2 class="font-bold text-xl">INTERCONSULTAS:</h2>
+                <br>
 
-        <!--Footer container-->
+                <p class="font-bold"> 
+                    CIMAD LaboraL cuenta con la posibilidad de ofrecer interconsultas con profesionales de primer nivel y amplia experiencia. 
+                </p>
+                <br>    
+                <p>    Contamos con las diferentes especialidades: 
+                    
+                    <li>	Psiquiatría </li>
+                    <li>	Cardiología </li>
+                    <li>	Oftalmología </li>
+                    <li>	Fonoaudiología </li>
+                    <li>	Neumonología </li>
+                    <li>	Neurología </li>
+                    <li>	Clínica </li>
+                    <li>	Ginecología </li>
+                    <li>	Dermatología </li>
+                    <li>	Cirugía general </li> </p>
+                </a>
+            </div>
+            <img class="relative rounded-md md:w-2/5 mt-8" src="{{asset('img/especialista01.jpg')}}" alt="">
+            
+        </div>
+
+        <div class="w-screen p-4 flex flex-col md:flex-row bg-gray-200 items-start justify-center animate-[wiggle_2s_ease-in_1]">
+            <img class="flex rounded-md md:w-2/5 mt-8" src="{{asset('img/medicos02.png')}}" alt="">
+            <div class="flex relative px-4 mx-4 md:ml-8 mt-8 py-3 lg:w-2/5 bg-gray-100/50 rounded-lg shadow-lg">
+                <a href="#">
+                    <h2 class="font-bold text-xl">AUSENTISMO</h2>
+                    <br><p>
+                        Control de Ausentismo
+                    </p>
+                    <br>
+                    <p>
+
+                        La Ley 20.744 de contrato de trabajo en sus artículos 209 y 210 le otorga el derecho al empleador de auditar las ausencias laborales por enfermedad y obliga al empleado a someterse a dicha auditoria.
+                        Teniendo en cuento lo mencionado, el control de ausentismo permite conocer el estado de salud del trabajador, estimar el tiempo de reingreso al trabajo y de esta forma realizar el organigrama productivo.
+                        Puede realizarse en nuestro centro médico, o bien en el domicilio del ausente.
+                    </p>
+                </a>
+            </div>
+            
+        </div>
+
+
 <footer
 class="flex flex-col items-center bg-neutral-200 text-center text-white dark:bg-neutral-600">
     <div class="container pt-9">
@@ -223,32 +268,14 @@ class="flex flex-col items-center bg-neutral-200 text-center text-white dark:bg-
                     d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                 </svg>
             </a>
-            <a href="#" class="mr-9 text-neutral-800 dark:text-neutral-200">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
-                    fill="currentColor"
-                    viewBox="0 0 24 24">
-                    <path
-                    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-                </svg>
-            </a>
-            <a href="#" class="text-neutral-800 dark:text-neutral-200">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
-                    fill="currentColor"
-                    viewBox="0 0 24 24">
-                    <path
-                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-            </a>
+            
+            
         </div>
     </div>
 
     <!--Copyright section-->
     <div
-    class="w-full bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+    class="w-full bg-neutral-200 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
     © 2023 Copyright:
     <a
         class="float-right text-neutral-800 dark:text-neutral-400"
